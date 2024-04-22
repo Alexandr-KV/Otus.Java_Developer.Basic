4df29472315371f3fd5b5e173ba47aea1ef5b265
Alexandr-KV <karandashovsasha@gmail.com>
2024-04-22 17:33:38 +0300

    Исправил названия методов и записал их в psvm используя рандомные аргументы

--- lesson_5/src/main/java/ru/otus/Hw_2.java
+++ lesson_5/src/main/java/ru/otus/Hw_2.java
@@ -1,20 +1,31 @@
 package ru.otus;
 
 
+import java.util.Arrays;
+
 public class Hw_2 {
     public static void main(String[] args) {
-
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        lineOutput((int) (Math.random()*10), "Hello world!");
+        sumOfTheElementsGreaterThan5(arr);
+        fillingTheArray((int)(Math.random()*10),arr);
+        System.out.println(Arrays.toString(arr));
+        incrementEachArrayElement((int)(Math.random()*10),arr);
+        System.out.println(Arrays.toString(arr));
+        arr[(int)(Math.random()*9)] = (int)(Math.random()*10);
+        //изменение рандомного элемента массива на рандомное число, чтобы вывод последнего метода не был одинаковым в каждом запуске
+        comparingHalvesOfArray(arr);
     }
 
 //реализация первого метода
-    public static void line_output(int number, String sentence) {
+    public static void lineOutput(int number, String sentence) {
         for (int i = 0; i < number; i++) {
             System.out.println(sentence);
         }
     }
 
 //реализация второго метода
-    public static void sum_of_the_elements_greater_than_5(int[] arr) {
+    public static void sumOfTheElementsGreaterThan5(int[] arr) {
         int summa = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > 5) {
@@ -25,21 +36,21 @@ public class Hw_2 {
     }
 
 //реализация третьего метода
-    public static void filling_the_array(int number, int[] arr) {
+    public static void fillingTheArray(int number, int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = number;
         }
     }
 
 //реализация четвертого метода
-    public static void increment_each_array_element(int number, int[] arr) {
+    public static void incrementEachArrayElement(int number, int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] += number;
         }
     }
 
 //реализация пятого метода
-    public static void comparing_halves_of_array(int[] arr) {
+    public static void comparingHalvesOfArray(int[] arr) {
         int summa1 = 0;
         int summa2 = 0;
         for (int i = 0; i < arr.length / 2; i++) {
