723c5fc0236e7a46dcdb2eddff81c92f884d86ea
Alexandr-KV <karandashovsasha@gmail.com>
2024-07-21 11:14:21 +0300

    Выполнил 23 домашнее задание

---
+++ Lesson_31/src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -0,0 +1,38 @@
+package ru.otus.java.basic.http.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+    private int port;
+    private Dispatcher dispatcher;
+
+    public HttpServer(int port) {
+        this.port = port;
+        this.dispatcher = new Dispatcher();
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен на порту: " + port);
+            while (true) {
+                new Thread(() -> {
+                    try (Socket socket = serverSocket.accept()) {
+                        byte[] buffer = new byte[8192];
+                        int n = socket.getInputStream().read(buffer);
+                        String rawRequest = new String(buffer, 0, n);
+                        HttpRequest request = new HttpRequest(rawRequest);
+                        request.printInfo(true);
+                        dispatcher.execute(request, socket.getOutputStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
